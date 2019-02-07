ee5e17b379ac52b9c1efefa186d24d74b9dc7ead
Derrik Curran <dcurran@kenzan.com>
2019-02-07 10:44:56 -0500

    Publish 0.1.0

---
+++ precache-manifest.76ee45ce281d144624511ae8ddb7ae64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9b03547f4f5017e49cc",
+    "url": "/static/js/main.e9b03547.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "e9b03547f4f5017e49cc",
+    "url": "/static/css/main.1eef92c1.chunk.css"
+  },
+  {
+    "revision": "2d5128e57c7f92da1520d33e499e0e6f",
+    "url": "/index.html"
+  }
+];
